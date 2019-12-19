880ea3b3afb61c7636512058d1276460b7d6e9b3
Emily Wu <huijiawu90@gmail.com>
2019-12-19 05:20:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,10 @@
 # shows everyone their current place in line 
+# E.g., "The line is currently: 1. Ada 2. Grace 3. Kent"
 def line(queue)
-  
+  if queue.empty?
+    puts "The line is currently empty."
+  else
+    q_string = "The line is currently:"
+    queue.
+  end
 end
